5931fbd7a381b55e3356360e2c9f88ce59068bee
Alexia <alexiamiha@icloud.com>
2022-02-09 15:57:38 +0000

    added the update method in my service class

--- src/main/java/com/qa/Nespresso/service/NespressoService.java
+++ src/main/java/com/qa/Nespresso/service/NespressoService.java
@@ -1,5 +1,7 @@
 package com.qa.Nespresso.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.qa.Nespresso.domain.Nespresso;
@@ -23,4 +25,20 @@ public class NespressoService {
     public Nespresso getByIDNespresso(Long id) {
 		return this.repo.getById(id);
 		}
+    
+     //update method 
+    public Nespresso updateNespresso(Long CoffeeId, Nespresso Nes) {
+		Optional<Nespresso>optionalNespresso=this.repo.findById(id);
+		if (optionalNespresso.isPresent()) {
+			Nespresso existingNespresso=optionalNespresso.get();
+			existingNespresso.setName(repo.getName());
+			existingNespresso.setType(repo.getType());
+			existingNespresso.setAroma(repo.getAroma());
+			existingNespresso.setIntensity(repo.getIntensity());
+			existingNespresso.setRoastiness(repo.getRoastiness());
+			return this.repo.save(existingNespresso);
+		} else {
+			return null;	
+		}
+    }
 }
