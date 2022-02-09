d5d2c6e6f611fd0787e7fcac3652376c9e0975e3
Alexia <alexiamiha@icloud.com>
2022-02-09 15:44:28 +0000

    added my create method in my service class

--- src/main/java/com/qa/Nespresso/service/NespressoService.java
+++ src/main/java/com/qa/Nespresso/service/NespressoService.java
@@ -2,7 +2,15 @@ package com.qa.Nespresso.service;
 
 import org.springframework.stereotype.Service;
 
+import com.qa.Nespresso.repo.NespressoRepo;
+
 @Service
 public class NespressoService {
+	
+	private NespressoRepo repo;
+
+	public NespressoService(NespressoRepo repo) {
+		this.repo = repo;
+	}
 
 }
