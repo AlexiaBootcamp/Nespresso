226c8463143a0440d938b66a6e6e12ed589d5b01
Alexia <alexiamiha@icloud.com>
2022-02-09 11:56:05 +0000

    initial commit

---
+++ src/main/java/com/qa/Nespresso/NespressoApplication.java
@@ -0,0 +1,13 @@
+package com.qa.Nespresso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NespressoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NespressoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/Nespresso/NespressoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.Nespresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NespressoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
