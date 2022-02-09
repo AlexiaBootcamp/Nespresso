1c3b4cd3eac2894db26ba295e2a59f6d0bfbb5b7
Alexia <alexiamiha@icloud.com>
2022-02-09 15:41:50 +0000

    added my create-mapping method in my controller class

--- src/main/java/com/qa/Nespresso/controller/NespressoController.java
+++ src/main/java/com/qa/Nespresso/controller/NespressoController.java
@@ -1,8 +1,11 @@
 package com.qa.Nespresso.controller;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.Nespresso.domain.Nespresso;
 import com.qa.Nespresso.service.NespressoService;
 
 @RestController
@@ -16,8 +19,12 @@ public class NespressoController {
 		this.s = s;
 	}
 	
-
-	
+   // create method 
+	@PostMapping("/create")
+	//passing a record with all thevalues//mapping the create method to the service class
+	public Nespresso createNespresso(@RequestBody Nespresso Nes) {
+		 return this.s.createNespresso(Nes);
+	}
 }
 	
 
