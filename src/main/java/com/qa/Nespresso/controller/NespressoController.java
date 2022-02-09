f075364277b70ae849d454dc76e8e260a8efad71
Alexia <alexiamiha@icloud.com>
2022-02-09 15:13:02 +0000

    declared my depandent class as private variable and generated the its constuctor

--- src/main/java/com/qa/Nespresso/controller/NespressoController.java
+++ src/main/java/com/qa/Nespresso/controller/NespressoController.java
@@ -3,9 +3,18 @@ package com.qa.Nespresso.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.Nespresso.service.NespressoService;
+
 @RestController
 public class NespressoController {
 	
+	//declare my variables and constructor
+	
+	private NespressoService s;
+
+	public NespressoController(NespressoService s) {
+		this.s = s;
+	}
 	
 
 	
