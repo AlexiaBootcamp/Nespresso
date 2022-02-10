ff7450a72fba557ed139b9ab6829a6d5c94f02d8
Alexia <alexiamiha@icloud.com>
2022-02-10 15:40:33 +0000

    added the createTest method in the ServiceTest class

--- src/test/java/Service/NespressoServiceTest.java
+++ src/test/java/Service/NespressoServiceTest.java
@@ -1,7 +1,46 @@
 package Service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.qa.Nespresso.repo.NespressoRepo;
+import com.qa.Nespresso.service.NespressoService;
 
+@SpringBootTest
 public class NespressoServiceTest {
+	
+	@Autowired
+	private NespressoService service;
+	
+	@MockBean
+	private NespressoRepo repo;
+	
+	@BeforeEach
+	void setUp() {
+		newNes = newNespresso("roma","espresso",7,"fruity","dark");
+		savedNes = newNespresso(1L,"roma","espresso",7,"fruity","dark");
+		
+		//System.out.println ("before");	
+	}
 
+	@Test
+	void testcreate() {
+		//System.out.println("create");	
+		//given
+		//testing data
+		
+		//when
+		Mockito.when(this.repo.save(newNes).thenReturn(savedNes);
+		//then
+		assertThat(this.service.create(newNes)).isEqualTo(savedNes);
+		//verify
+		Mockito.verify(this.repo, Mockito.times(1)).save(newNes);
+		
+	}
+	
+	
 }
